780f108250fa55c4cf3a718c0c5acfa111fc3edf
Hu <marcus_hu@outlook.com>
2022-09-29 18:01:36 -0500

    Project Initialize

---
+++ common/service-util/src/main/java/com/hyc/App.java
@@ -0,0 +1,13 @@
+package com.hyc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
