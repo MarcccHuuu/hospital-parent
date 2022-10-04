005186d42a203baad783d86186df5cb241211104
Hu <marcus_hu@outlook.com>
2022-10-04 17:39:44 -0500

    Configure Swagger2

--- service/service-hosp/src/main/java/com/hyc/clinicsystem/hosp/controller/HospitalSetController.java
+++ service/service-hosp/src/main/java/com/hyc/clinicsystem/hosp/controller/HospitalSetController.java
@@ -2,13 +2,14 @@ package com.hyc.clinicsystem.hosp.controller;
 
 import com.hyc.clinicsystem.hosp.service.HospitalSetService;
 import com.hyc.clinicsystem.model.hosp.HospitalSet;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = "医院设置管理")
 @RestController
 @RequestMapping("/admin/hosp/hospitalSet")
 public class HospitalSetController {
@@ -18,9 +19,17 @@ public class HospitalSetController {
 
     // 查询医院表所有信息
     // http://localhost:8201/admin/hosp/hospitalSet/findAll
+    @ApiOperation(value = "获取所有医院设置")
     @GetMapping("findAll")
     public List<HospitalSet> findAllHospitalSet() {
         return hospitalSetService.list();
+    }
+
+    // 逻辑删除医院设置
+    @ApiOperation(value = "逻辑删除医院设置")
+    @DeleteMapping("{id}")
+    public boolean removeHospitalSet(@PathVariable Long id) {
+        return hospitalSetService.removeById(id);
     }
 
 }
