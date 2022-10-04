6a6e581d3ba911bbb13648f6d6c8fce92e7741ac
Hu <marcus_hu@outlook.com>
2022-10-04 16:53:21 -0500

    Test API Controller

--- service/service-hosp/src/main/java/com/hyc/clinicsystem/hosp/controller/HospitalSetController.java
+++ service/service-hosp/src/main/java/com/hyc/clinicsystem/hosp/controller/HospitalSetController.java
@@ -1,10 +1,13 @@
 package com.hyc.clinicsystem.hosp.controller;
 
 import com.hyc.clinicsystem.hosp.service.HospitalSetService;
+import com.hyc.clinicsystem.model.hosp.HospitalSet;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/hosp/hospitalSet")
@@ -12,5 +15,12 @@ public class HospitalSetController {
 
     @Autowired
     private HospitalSetService hospitalSetService;
+
+    // 查询医院表所有信息
+    // http://localhost:8201/admin/hosp/hospitalSet/findAll
+    @GetMapping("findAll")
+    public List<HospitalSet> findAllHospitalSet() {
+        return hospitalSetService.list();
+    }
 
 }
